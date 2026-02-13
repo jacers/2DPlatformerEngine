66cbd124d4a6c17e49d9e51249d81be19a554797
jacers <jacerausch@gmail.com>
2026-02-13 10:55:27 -0600

    Modified zoom to match NSMBU, made it easier to adjust zoom by tiles

--- core/constants.lua
+++ core/constants.lua
@@ -1,15 +1,15 @@
-SCREEN            = {
+TILE_LENGTH  = 16 -- Length of a square tile in pixels
+
+SCREEN       = {
     WIDTH = 1280,
     HEIGHT = 720,
     COLOR = { 28 / 255, 3 / 255, 51 / 255, 1 }
 }
 
-MAX_FRAME_DT      = 0.1
-PHYSICS_STEP      = 1 / 120
+MAX_FRAME_DT = 0.1
+PHYSICS_STEP = 1 / 120
 
-EDITOR_MOVE_SPEED = 200
-
-PLAYER            = {
+PLAYER       = {
     WIDTH               = 16,
     HEIGHT              = 16,
     WALK_SPEED          = 90,
@@ -31,8 +31,8 @@ PLAYER            = {
     HITBOX_INSET        = 1,
 }
 
-CAMERA            = {
-    DEFAULT_ZOOM     = 2.64, -- how zoomed in the camera starts
+CAMERA       = {
+    DEFAULT_ZOOM     = SCREEN.WIDTH / (24 * TILE_LENGTH), -- Constant = tiles wide
     MIN_ZOOM         = 0.75,
     MAX_ZOOM         = 6.0,
 
@@ -60,7 +60,7 @@ CAMERA            = {
     }
 }
 
-DEBUG             = {
+DEBUG        = {
     -- Draw player's hitbox in red on top for clarity
     DRAW_PLAYER_HITBOX = false,
     HITBOX_FILL_ALPHA  = 0.15, -- How easy it is to see
